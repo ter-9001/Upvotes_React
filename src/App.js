17bf82e048a7b73d3d9502a5bab09b6007808db7
martins <gabrielmartinsgmgm23@gmail.com>
2021-10-02 20:00:56 -0300

    Atualizando Layouts

--- src/App.js
+++ src/App.js
@@ -45,42 +45,31 @@ class App extends Component {
     comentarios[index].score += 1;
 
   
-    this.setState({comentarios: comentarios})
+    this.setState({comentarios: comentarios});
     
   }
   enviarComentario()
   {
+
+   (!arguments[0] || !arguments[1]) ?  alert("Erro: usuário e/ou comentário\n não digitados!!!!")
+    : alert("Enviando comentário!!!!"); 
     
-    if(!arguments[0])
-    {
-      alert("Digite o usuário!!!!");
-    }
-    else if(!arguments[1])
-    {
-      alert("Digite o cometário!!!!");
-    }
-    else if(!arguments[0] && !arguments[1])
-    {
-
-    }
-    else
-    {
-
-            let novoCometario = {
-              usuario: arguments[0],
-              comentario: arguments[1],
-              score: 0
-            };
-        
-          
-        
-            let comentarios = this.state.cometarios;
-        
-            comentarios.push(novoCometario)
-        
-            this.setState({comentarios: comentarios})
-        
-      }
+        {
+                let novoCometario = {
+                  usuario: arguments[0],
+                  comentario: arguments[1],
+                  score: 0
+                };
+            
+              
+            
+                let comentarios = this.state.cometarios;
+            
+                comentarios.push(novoCometario);
+            
+                this.setState({comentarios: comentarios});
+            
+          }
     
 
   }
@@ -90,7 +79,7 @@ class App extends Component {
     let comentario = this.ordenar();
 
     return(
-               <div>
+               <div style={{backgroundColor:''}}>
                      <div className={"container-fluid"} style={{display: 'flex', flexWrap: 'row', justifyContent: 'center', backgroundColor: '#ff0000', margin:'5px'}}>
 
                          <div>
@@ -106,43 +95,43 @@ class App extends Component {
                     </div>
                
                
-                    <div style={{display:'flex',justifyContent:'center'}}>
+                    <div className={"container-fluid"} style={{display:'flex',justifyContent:'center'}}>
                     <div style={{display:'flex', flexDirection: 'column'}}> 
                 
                                       {comentario.map(
                                         (obj, index) => {
 
                                           return(
-                                              <div className={'text-center'} 
-                                              style={{width: '400px', border:'0.4px solid #828080', marginTop:'15px'}}> 
+                                          <div className={'text-center'} 
+                                          style={{width: '400px', border:'0.4px solid #828080', marginTop:'15px'}}> 
 
-                                                        <div style={{display:'flex', flexWrap:'row',
-                                                      backgroundColor:'#309bb1', color:'white', justifyContent:'space-between'}}>
-                                                          
-                                                          <p style={{marginBottom: '0', marginLeft: '15px'}} 
-                                                          className={'espaco_1linha'}> {obj.usuario} </p>
+                                                <div style={{display:'flex', flexWrap:'row',
+                                              backgroundColor:'#309bb1', color:'white', justifyContent:'space-between'}}>
+                                                  
+                                                    <p style={{marginBottom: '0', marginLeft: '15px'}} 
+                                                    className={'espaco_1linha'}> {obj.usuario} </p>
 
-                                                        <div style={{display:'flex', flexWrap:'row'}}>
+                                                <div style={{display:'flex', flexWrap:'row'}}>
 
-                                                              <p style={{marginBottom: '0', marginRight: '15px'}} 
-                                                              > {obj.score} </p>
+                                                    <p style={{marginBottom: '0', marginRight: '15px'}} 
+                                                    > {obj.score} </p>
 
-                                                              <p className={'darLike'} style={{marginBottom: '0', marginRight: '15px', fontSize: '10px'}} 
-                                                              onClick={() => this.scoreAumentar(index)}> Upvote here </p>
-
-                                                        </div>
-
-
-                                                        </div>
-
-                                                        <div >
-
-                                                            <p style={{fontSize: '30px', color: '#828080'}}
-                                                            > {obj.comentario} </p>
-
-                                                        </div>
+                                                    <p className={'darLike'} style={{marginBottom: '0', marginRight: '15px', fontSize: '10px'}} 
+                                                    onClick={() => this.scoreAumentar(index)}> Upvote here </p>
 
                                                 </div>
+
+
+                                                </div>
+
+                                                <div >
+
+                                                    <p style={{fontSize: '30px', color: '#828080'}}
+                                                    > {obj.comentario} </p>
+
+                                                </div>
+
+                                            </div>
                                           )
                                         }
                                       )}
